4f3875afcc45ca93b935df3b7357072210361bac
lyon <1765813715@qq.com>
2021-06-24 17:42:11 +0800

    finish add and edit

--- ftplugin/latex/Inkscape.py
+++ ftplugin/latex/Inkscape.py
@@ -1,4 +1,6 @@
 import warnings
+import vim
+import re
 import subprocess
 from pathlib import Path
 from shutil import copy
@@ -18,6 +20,31 @@ def inkscape(path):
         # warning in Python3.7+
         warnings.simplefilter("ignore", ResourceWarning)
         subprocess.Popen(['inkscape', str(path)])
+
+
+def latex_template(name, title):
+    return [r"\begin{figure}[ht]",
+            r"    \centering",
+            rf"    \incfig{{{name}}}",
+            rf"    \caption{{{title}}}",
+            rf"    \label{{fig:{name}}}",
+            r"\end{figure}"]
+
+
+def setup_template():
+    return [
+        r"% figure support",
+        r"\usepackage{import}",
+        r"\usepackage{pdfpages}",
+        r"\usepackage{transparent}",
+        r"\usepackage{xcolor}",
+        r"",
+        r"\newcommand{\incfig}[2][1]{",
+        r"    \def\svgwidth{#1\columnwidth}",
+        r"    \import{./figures/}{#2.pdf_tex}",
+        r"}"
+
+    ]
 
 
 def create(title, root):
@@ -46,7 +73,31 @@ def create(title, root):
 
     # Print the code for including the figure to stdout.
     # Copy the indentation of the input.
+    # print(indent(latex_template(figure_path.stem, title), indentation=leading_spaces))
+    # vim.current.line = indent(latex_template(
+    # figure_path.stem, title), indentation=leading_spaces)
+    r = vim.current.range
+    r.append(latex_template(figure_path.stem, title))
+    del vim.current.line
+
+    print("Created figure: ", figure_path)
 
 
-def Inkscape_example():
-    return template
+def get_title(title_line):
+    regex = r"(?:incfig{)(.*?)(?:})"
+    match = re.search(regex, title_line, flags=0)
+    return match.group(1)
+
+
+def edit(title, root):
+    title = title.strip()
+    file_name = title.replace(' ', '-').lower() + '.svg'
+    figures = Path(root).absolute()
+    figure_path = figures / file_name
+    inkscape(figure_path)
+
+    print("Edit figure: ", figure_path)
+
+
+def setup():
+    vim.current.range.append(setup_template())
--- ftplugin/latex/Inkscape.vim
+++ ftplugin/latex/Inkscape.vim
@@ -5,6 +5,17 @@ python3 import sys
 python3 import vim
 python3 sys.path.append(vim.eval('expand("<sfile>:h")'))
 
+if !exists("g:inkscape_fig_template")
+let g:inkscape_fig_template = "
+	\ begin{figure}
+	\ centering
+	\ begin{figure}[ht]
+	\ incfig{<+name+>}
+	\ chaption{<+title+>}
+	\ label{fig:<+name+>}
+	\ end{figure}"
+endif
+
 " --------------------------------
 "  Function(s)
 " --------------------------------
@@ -21,7 +32,32 @@ create(title,root)
 endOfPython
 endfunction
 
+function! SetupFig()
+python3 << endOfPython
+from Inkscape import setup
+setup()
+endOfPython
+endfunction
+
+
+function! EditFig()
+python3 << endOfPython
+from Inkscape import get_title,edit
+
+title_line = vim.current.line
+title = get_title(title_line)
+root = vim.eval("b:vimtex.root")
+root = root+'/figures'
+
+edit(title,root)
+
+endOfPython
+endfunction
+
 " --------------------------------
 "  Expose our commands to the user
 " --------------------------------
 command! CreateFig call CreateFig()
+command! SetupFig call SetupFig()
+command! EditFig call EditFig()
+
